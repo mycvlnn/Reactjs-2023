18b66ff105fb7103cc7fb07d90da6ea4bfcca443
Lê Ngoại Ngữ <lengoaingu@gmail.com>
2023-07-25 20:32:45 +0700

    Outputting Conditional Content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
-import expenses from "./constants/Expense";
+import DUMMY_EXPENSES from "./constants/Expense";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+    const addExpenseHandler = (expense) => {
+        setExpenses((prevExpenses) => {
+            return [expense, ...prevExpenses];
+        });
+    };
+
     return (
         <div className="App">
+            <NewExpense onAddExpense={addExpenseHandler} />
             <Expenses expenses={expenses} />
         </div>
     );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,20 +1,38 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses({ expenses = [] }) {
+    const [filteredYear, setFilteredYear] = useState("2020");
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = expenses.filter(
+        (expense) => expense.date.getFullYear().toString() === filteredYear
+    );
+
+    let expenseContent = <div className="not-found">No expenses found!</div>;
+
+    if (filteredExpenses.length > 0)
+        expenseContent = filteredExpenses.map((expense) => {
+            return (
+                <ExpenseItem
+                    key={expense.id}
+                    date={expense.date}
+                    price={expense.amount}
+                    title={expense.title}
+                />
+            );
+        });
+
     return (
         <Card className="expenses">
-            {expenses.map((expense) => {
-                return (
-                    <ExpenseItem
-                        key={expense.id}
-                        date={expense.date}
-                        price={expense.amount}
-                        title={expense.title}
-                    />
-                );
-            })}
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+            {expenseContent}
         </Card>
     );
 }
